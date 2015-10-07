3fcf8d052144dc027d632f4fd2f5090d553fbd68
João Silva <joaosilva@revtut.net>
2015-10-07 10:18:37 +0100

    Color scroll allows to choose two colors for base text.

--- src/net/revtut/libraries/text/scroller/ColorScroller.java
+++ src/net/revtut/libraries/text/scroller/ColorScroller.java
@@ -13,9 +13,14 @@ public class ColorScroller extends Scroller {
     private final int length;
 
     /**
-     * Base color of the text
+     * Base 1 color of the text
      */
-    private final ChatColor base;
+    private ChatColor base1;
+
+    /**
+     * Base 2 color of the text
+     */
+    private ChatColor base2;
 
     /**
      * Scroll color of the text
@@ -26,16 +31,18 @@ public class ColorScroller extends Scroller {
      * Constructor of TextScroller
      * @param text text of the scroller
      * @param length length of the scroller
-     * @param base base color of the text
+     * @param base1 base 1 color of the text
+     * @param base2 base 2 color of the text
      * @param scroll scroll color of the text
      */
-    public ColorScroller(final String text, final int length, final ChatColor base, final ChatColor scroll) {
+    public ColorScroller(final String text, final int length, final ChatColor base1, final ChatColor base2, final ChatColor scroll) {
         super(text);
 
         if(length > text.length())
             throw new IllegalArgumentException("Length is bigger than the text to scroll");
         this.length = length;
-        this.base = base;
+        this.base1 = base1;
+        this.base2 = base2;
         this.scroll = scroll;
     }
 
@@ -54,13 +61,18 @@ public class ColorScroller extends Scroller {
         builder.insert(position, scroll);
         int endPosition = (position + 2) + length; // Position + 2 because a color is two characters
         if(endPosition >= builder.length()) {
+            // Swap colors
+            ChatColor temp = base1;
+            base1 = base2;
+            base2 = temp;
+
             endPosition -= builder.length();
 
-            builder.insert(endPosition, base);
+            builder.insert(endPosition, base2);
             builder.insert(0, scroll);
         } else {
-            builder.insert(endPosition, base);
-            builder.insert(0, base);
+            builder.insert(endPosition, base2);
+            builder.insert(0, base1);
         }
 
 
